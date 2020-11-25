b775be8a7664f73b2d0cfff69472145ebfb4f1b4
Alexandre Moreira <4lexandre.moreira@gmail.com>
2020-11-25 11:13:50 -0300

    State Begin

---
+++ src/components/State.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export default function State(){
+    const [count, setCount] = useState(0);
+}
+
+return (
+    <>
+        <h1>Count: {}</h1>
+    </>
+)
